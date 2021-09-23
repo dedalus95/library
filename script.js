d06b6757f6b9b8a430788b6be1f441663378c44e
Emanuele <ecerilli1@gmail.com>
2021-09-23 18:27:53 +0200

    you have to figure out how to create a new table row every time you add a book.

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const btn = document.getElementById('btn');
 btn.innerHTML = 'sto cazzo';
+const form = document.getElementById('book-form');
 
 function showForm() {
-  const form = document.getElementById('book-form');
   form.classList.remove('hide-form');
-  form.classList.add('display-form');
 }
 
-btn.addEventListener('mousedown', function() {
-  showForm();
-});
+function showTable() {
+  let tableHeader = document.getElementById('table-header');
+  let tableRow = document.getElementById('table-row');
+  tableHeader.classList.remove('hidden');
+  tableRow.classList.remove('hidden');
+}
 
+btn.addEventListener('mousedown', showForm);
 
 let myLibrary = [];
 
@@ -22,21 +25,42 @@ function Book(title, author, pages, read)
   this.read = read 
 }
 
-function addBookToLibrary() {
-  
-  let a = new Book('Der Zauberberg', 'Thomas Mann', 767, 'read');
+let a = document.getElementById('submitBtn').addEventListener('click', function(event) {
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const pages = document.getElementById('pages');
+  const read = document.getElementById('read');
+  event.preventDefault();
+  // cleanArray(myLibrary);
+  addBookToLibrary(title.value, author.value, pages.value, read.value)
+  showTable();
+  form.reset();
+  console.log(myLibrary);
+});
 
-  myLibrary.push(`${a.title}, ${a.author}, ${a.pages}, ${a.read}.`);
+function addBookToLibrary(title, author, pages, read) {
+  a = new Book(title, author, pages, read);
+  myLibrary.unshift(a.title, a.author, a.pages, a.read);
+  loopsThroughTheArray();
+}
+
+function cleanArray(array) {
+  array.slice(4,0);
 }
 
 
 function loopsThroughTheArray() {
-  for(let i = 0; i < myLibrary.length; i++) {
-    document.body.innerHTML += `${myLibrary[i]} `;
+
+  let one = document.getElementById('1');
+  let two = document.getElementById('2');
+  let three = document.getElementById('3');
+  let four = document.getElementById('4');
+    one.innerHTML += `${myLibrary[0]} `;
+    two.innerHTML += `${myLibrary[1]} `;
+    three.innerHTML += `${myLibrary[2]} `;
+    four.innerHTML += `${myLibrary[3]} `;
     }
-}
 
 
 
-addBookToLibrary()
-loopsThroughTheArray()
+
